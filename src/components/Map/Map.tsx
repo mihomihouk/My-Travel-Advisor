9625dcdff753ceeeba4d0db3c7f7f2a79f741395
mihomihouk <mihomihouk@gmail.com>
2022-07-04 14:20:43 +0100

    Added restaurant info on the map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Bounds, Coordinates } from '../../types/type'
 
@@ -6,18 +6,26 @@ import { Bounds, Coordinates } from '../../types/type'
 import GoogleMapReact from 'google-map-react'
 import { Paper, Typography, useMediaQuery } from '@material-ui/core'
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
-import Rating from '@material-ui/lab'
+import Rating from '@material-ui/lab/Rating'
 import useStyles from './styles'
 
 interface Props {
   setCoordinates: (coordinates: Coordinates) => void
   setBounds: (bounds: Bounds) => void
   coordinates: Coordinates
+  places: any
+  setChildClicked: (child: any) => void
 }
-const Map: React.FC<Props> = ({ setCoordinates, setBounds, coordinates }) => {
+const Map: React.FC<Props> = ({
+  setCoordinates,
+  setBounds,
+  coordinates,
+  places,
+  setChildClicked,
+}) => {
   const classes = useStyles()
-  //false if width of device is lager than 600px
-  const isMobile = useMediaQuery('(min-width:600px)')
+  //true if width of device is lager than 600px
+  const isDesktop = useMediaQuery('(min-width:600px)')
 
   return (
     <div className={classes.mapContainer}>
@@ -32,8 +40,41 @@ const Map: React.FC<Props> = ({ setCoordinates, setBounds, coordinates }) => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng })
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
         }}
-        // onChildClick={''}
-      ></GoogleMapReact>
+        onChildClick={(child) => {
+          setChildClicked(child)
+        }}
+      >
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            // as data from API is string, convert it here into Numbers
+            // @ts-ignore
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!isDesktop ? (
+              <LocationOnOutlinedIcon color="primary" fontSize="large" />
+            ) : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography variant="subtitle2" gutterBottom>
+                  {place.name}
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  src={
+                    place.photo
+                      ? place.photo.images.large.url
+                      : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'
+                  }
+                  alt={place.name}
+                />
+                <Rating size="small" value={Number(place.rating)} readOnly />
+              </Paper>
+            )}
+          </div>
+        ))}
+      </GoogleMapReact>
     </div>
   )
 }
